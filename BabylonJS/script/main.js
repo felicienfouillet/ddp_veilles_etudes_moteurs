631a16570e49d37a498e93dfba3e8c09f744dcfd
Felicien <felicienfouillet@live.fr>
2020-04-09 17:39:35 +0200

    evo: test of the render engine

--- BabylonJS/script/main.js
+++ BabylonJS/script/main.js
@@ -1,42 +1,37 @@
 window.addEventListener('DOMContentLoaded', function() {
-    // All the following code is entered here.
+    var canvas = document.getElementById('canvas');
 
-    var canvas = document.getElementById('renderCanvas');
     var engine = new BABYLON.Engine(canvas, true);
-
 
     var createScene = function() {
         var scene = new BABYLON.Scene(engine);
-
         scene.clearColor = new BABYLON.Color3.White();
 
         var box = BABYLON.Mesh.CreateBox("Box", 4.0, scene);
-        //var box2 = BABYLON.Mesh.CreateBox("Box2", 4.0, scene);
 
-        var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 0, -10), scene);
-        camera.setTarget(BABYLON.Vector3.Zero());
+        // Create a second object so we can actually witness the movement
+        // Make this one wireframe to distiguish the difference.
+        var box2 = BABYLON.Mesh.CreateBox("Box2", 4.0, scene);
+        var material = new BABYLON.StandardMaterial("material1", scene);
+        material.wireframe = true;
+        box2.material = material;
+
+        box2.position = new BABYLON.Vector3(0, 5, 0);
+
+        var camera = new BABYLON.FollowCamera("followCam", BABYLON.Vector3.Zero(), scene);
+        camera.target = box;
+        camera.radius = 100;
+        camera.heightOffset = 0;
         camera.attachControl(canvas, true);
-        camera.keysUp.push(90);
-        camera.keysDown.push(83);
-        camera.keysRight.push(68);
-        camera.keysLeft.push(81);
 
-        /*var rotateCamera = new BABYLON.ArcRotateCamera("arcCam",
-            BABYLON.Tools.ToRadians(45),
-            BABYLON.Tools.ToRadians(45),
-            20.0, box.position, scene);
-        rotateCamera.attachControl(canvas, true);
-        rotateCamera.keysUp.push(38);
-        rotateCamera.keysDown.push(40);
-        rotateCamera.keysRight.push(39);
-        rotateCamera.keysLeft.push(37);*/
-
+        scene.activeCamera = camera;
         return scene;
     }
 
     var scene = createScene();
-
     engine.runRenderLoop(function() {
+        scene.getMeshByName("Box").position.y += 0.1;
+        scene.getMeshByName("Box").position.x += 0.1;
         scene.render();
     });
 });
