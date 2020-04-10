613fc481fe9b736f3f1badd701d1556f1ec4c833
Felicien <felicienfouillet@live.fr>
2020-04-10 18:17:20 +0200

    evo: addition of the player and basic movements

--- Phaser/script/main.js
+++ Phaser/script/main.js
@@ -4,6 +4,13 @@ var config = {
     height: 432,
     backgroundColor: '#2d2d2d',
     pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -23,37 +30,92 @@ function preload() {
     this.load.spritesheet('platform', 'tileset.png', { frameWidth: 16, frameHeight: 16 });
     this.load.spritesheet('props', 'props.png', { frameWidth: 16, frameHeight: 16 });
 
-    // this.load.spritesheet('character',
-    //     'assets/characters/adventurer-v1.5-Sheet.png', { frameWidth: 32, frameHeight: 48 }
-    // );
+    /*this.load.spritesheet('character',
+        '../characters/individual_sprites/adventurer-air-attack1-00.png', { frameWidth: 50, frameHeight: 37 }
+    );*/
+    this.load.spritesheet('character',
+        '../characters/adventurer-v1.5-Sheet.png', { frameWidth: 50, frameHeight: 37 }
+    );
 }
+
+var player;
+var cursors;
 
 function create() {
     this.map = this.make.tilemap({ key: 'map' });
 
     var groundTiles = this.map.addTilesetImage('platform', 'platform');
     var propsTiles = this.map.addTilesetImage('props', 'props');
-    var backgroundTiles = this.add.tileSprite(0, 0, 384, 216, 'background').setScale(2);
 
-    var groundLayer = this.map.createStaticLayer('platform', groundTiles, 0, 0);
-    var propsLayer = this.map.createStaticLayer('props', propsTiles, 0, 0);
+    for (i = 384; i < this.map.widthInPixels; i += 768) {
+        var backgroundTiles = this.add.tileSprite(i, 216, 384, 216, 'background').setScale(2);
+    }
 
-    // this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    var backgroundPropsLayer = this.map.createStaticLayer('background_props', propsTiles, 0, 20).setScale(2);
+    var propsLayer = this.map.createStaticLayer('props', propsTiles, 0, 20).setScale(2);
+    var groundLayer = this.map.createStaticLayer('platform', groundTiles, 0, 20).setScale(2);
 
-    // var cursors = this.input.keyboard.createCursorKeys();
 
-    // var controlConfig = {
-    //     camera: this.cameras.main,
-    //     left: cursors.left,
-    //     right: cursors.right,
-    //     up: cursors.up,
-    //     down: cursors.down,
-    //     speed: 0.5
-    // };
+    player = this.physics.add.sprite(0, 200, 'character').setScale(2);
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
 
-    // controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
+    this.anims.create({
+        key: 'idle',
+        frames: this.anims.generateFrameNumbers('character', { start: 0, end: 4 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('character', { start: 8, end: 13 }),
+        frameRate: 10,
+        revert: true,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('character', { start: 8, end: 13 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    var controlConfig = {
+        camera: this.cameras.main,
+        left: cursors.left,
+        right: cursors.right,
+        up: cursors.up,
+        down: cursors.down,
+        speed: 0.5
+    };
+
+    controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
 }
 
 function update(time, delta) {
-    //    controls.update(delta);
+    controls.update(delta);
+
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    } else {
+        player.setVelocityX(0);
+
+        player.anims.play('idle');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-330);
+    }
 }
