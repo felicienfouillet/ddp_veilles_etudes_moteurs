09d9a27c80c1f9bd7ef02daf559d26a867e21c34
Felicien <felicienfouillet@live.fr>
2020-04-14 12:52:16 +0200

    evo: Modification of the player's mouvements and animation and modification of camera

--- Phaser/script/main.js
+++ Phaser/script/main.js
@@ -1,6 +1,6 @@
 var config = {
     type: Phaser.AUTO,
-    width: 782,
+    width: 750,
     height: 432,
     backgroundColor: '#2d2d2d',
     pixelArt: true,
@@ -18,7 +18,12 @@ var config = {
     }
 };
 
+var player;
+var cursors;
 var controls;
+var direction;
+var isJumping;
+
 
 var game = new Phaser.Game(config);
 
@@ -30,16 +35,14 @@ function preload() {
     this.load.spritesheet('platform', 'tileset.png', { frameWidth: 16, frameHeight: 16 });
     this.load.spritesheet('props', 'props.png', { frameWidth: 16, frameHeight: 16 });
 
-    /*this.load.spritesheet('character',
-        '../characters/individual_sprites/adventurer-air-attack1-00.png', { frameWidth: 50, frameHeight: 37 }
-    );*/
-    this.load.spritesheet('character',
-        '../characters/adventurer-v1.5-Sheet.png', { frameWidth: 50, frameHeight: 37 }
+    this.load.spritesheet('characterRight',
+        '../characters/adventurer-v1.5-Sheet-right.png', { frameWidth: 50, frameHeight: 37 }
+    );
+
+    this.load.spritesheet('characterLeft',
+        '../characters/adventurer-v1.5-Sheet-left.png', { frameWidth: 50, frameHeight: 37 }
     );
 }
-
-var player;
-var cursors;
 
 function create() {
     this.map = this.make.tilemap({ key: 'map' });
@@ -56,20 +59,26 @@ function create() {
     var groundLayer = this.map.createStaticLayer('platform', groundTiles, 0, 20).setScale(2);
 
 
-    player = this.physics.add.sprite(0, 200, 'character').setScale(2);
+    player = this.physics.add.sprite(50, 325, 'characterRight').setScale(2);
     player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
+    direction = 'right';
 
     this.anims.create({
-        key: 'idle',
-        frames: this.anims.generateFrameNumbers('character', { start: 0, end: 4 }),
+        key: 'idleRight',
+        frames: this.anims.generateFrameNumbers('characterRight', { start: 0, end: 4 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'idleLeft',
+        frames: this.anims.generateFrameNumbers('characterLeft', { start: 4, end: 7 }),
         frameRate: 10,
         repeat: -1
     });
 
     this.anims.create({
         key: 'left',
-        frames: this.anims.generateFrameNumbers('character', { start: 8, end: 13 }),
+        frames: this.anims.generateFrameNumbers('characterLeft', { start: 8, end: 12 }),
         frameRate: 10,
         revert: true,
         repeat: -1
@@ -77,20 +86,34 @@ function create() {
 
     this.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('character', { start: 8, end: 13 }),
+        frames: this.anims.generateFrameNumbers('characterRight', { start: 8, end: 13 }),
         frameRate: 10,
         repeat: -1
     });
 
-    cursors = this.input.keyboard.createCursorKeys();
+    this.anims.create({
+        key: 'jumpRight',
+        frames: this.anims.generateFrameNumbers('characterRight', { start: 14, end: 29 }),
+        frameRate: 15,
+        repeat: 0
+    });
+
+    this.anims.create({
+        key: 'jumpLeft',
+        frames: this.anims.generateFrameNumbers('characterLeft', { start: 8, end: 13 }),
+        frameRate: 15,
+        repeat: 0
+    });
 
     var camera = this.cameras.main;
     camera.startFollow(player);
-    camera.setBounds(0, 0, 1400, 216);
+    camera.setBounds(0, 0, 14000, 216);
 
-    groundLayer.setCollisionBetween(1, 0);
+    groundLayer.setCollisionByExclusion([-1])
     this.physics.add.collider(player, groundLayer);
-    player.setCollideWorldBounds(true);
+    player.setCollideWorldBounds(false);
+
+    cursors = this.input.keyboard.createCursorKeys();
 
     var controlConfig = {
         camera: this.cameras.main,
@@ -98,6 +121,7 @@ function create() {
         right: cursors.right,
         up: cursors.up,
         down: cursors.down,
+        space: cursors.space,
         speed: 0.5
     };
 
@@ -105,23 +129,33 @@ function create() {
 }
 
 function update(time, delta) {
-    //controls.update(delta);
+    controls.update(delta);
 
     if (cursors.left.isDown) {
+        direction = 'left';
         player.setVelocityX(-160);
 
         player.anims.play('left', true);
     } else if (cursors.right.isDown) {
+        direction = 'right';
         player.setVelocityX(160);
 
-        player.anims.play('right', true);
+        if (isJumping == true) {
+            player.anims.play('jumpRight', true);
+        } else {
+            player.anims.play('right', true);
+        }
     } else {
         player.setVelocityX(0);
-
-        player.anims.play('idle');
+        if (direction == 'right') {
+            player.anims.play('idleRight');
+        } else if (direction == 'left') {
+            player.anims.play('idleLeft');
+        }
     }
 
-    if (cursors.up.isDown) {
-        player.setVelocityY(-100);
+    if (cursors.space.isDown && player.body.onFloor()) {
+        isJumping = true;
+        player.setVelocityY(-200);
     }
 }
