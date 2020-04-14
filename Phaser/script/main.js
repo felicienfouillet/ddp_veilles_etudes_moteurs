29bc66e2fcd957e7c7da664f043f72600769d895
Felicien <felicienfouillet@live.fr>
2020-04-14 17:42:34 +0200

    evo modifiction of animation using 'flipX' and addition of the slide animation after jump

--- Phaser/script/main.js
+++ Phaser/script/main.js
@@ -21,8 +21,8 @@ var config = {
 var player;
 var cursors;
 var controls;
-var direction;
 var isJumping;
+var isFalling;
 
 
 var game = new Phaser.Game(config);
@@ -35,12 +35,8 @@ function preload() {
     this.load.spritesheet('platform', 'tileset.png', { frameWidth: 16, frameHeight: 16 });
     this.load.spritesheet('props', 'props.png', { frameWidth: 16, frameHeight: 16 });
 
-    this.load.spritesheet('characterRight',
-        '../characters/adventurer-v1.5-Sheet-right.png', { frameWidth: 50, frameHeight: 37 }
-    );
-
-    this.load.spritesheet('characterLeft',
-        '../characters/adventurer-v1.5-Sheet-left.png', { frameWidth: 50, frameHeight: 37 }
+    this.load.spritesheet('character',
+        '../characters/adventurer-v1.5-Sheet.png', { frameWidth: 50, frameHeight: 37 }
     );
 }
 
@@ -59,49 +55,41 @@ function create() {
     var groundLayer = this.map.createStaticLayer('platform', groundTiles, 0, 20).setScale(2);
 
 
-    player = this.physics.add.sprite(50, 325, 'characterRight').setScale(2);
-    player.setBounce(0.2);
-    direction = 'right';
+    player = this.physics.add.sprite(50, 325, 'character').setScale(2);
+    //player.setBounce(0.2);
 
     this.anims.create({
-        key: 'idleRight',
-        frames: this.anims.generateFrameNumbers('characterRight', { start: 0, end: 4 }),
-        frameRate: 10,
-        repeat: -1
-    });
-    this.anims.create({
-        key: 'idleLeft',
-        frames: this.anims.generateFrameNumbers('characterLeft', { start: 4, end: 7 }),
+        key: 'idle',
+        frames: this.anims.generateFrameNumbers('character', { start: 0, end: 4 }),
         frameRate: 10,
         repeat: -1
     });
 
     this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('characterLeft', { start: 8, end: 12 }),
+        key: 'run',
+        frames: this.anims.generateFrameNumbers('character', { start: 8, end: 12 }),
         frameRate: 10,
         revert: true,
         repeat: -1
     });
 
     this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('characterRight', { start: 8, end: 13 }),
+        key: 'jump',
+        frames: this.anims.generateFrameNumbers('character', { start: 13, end: 21 }),
+        frameRate: 15,
+        repeat: 0
+    });
+    this.anims.create({
+        key: 'slide',
+        frames: this.anims.generateFrameNumbers('character', { start: 24, end: 28 }),
         frameRate: 10,
         repeat: -1
     });
 
     this.anims.create({
-        key: 'jumpRight',
-        frames: this.anims.generateFrameNumbers('characterRight', { start: 14, end: 29 }),
-        frameRate: 15,
-        repeat: 0
-    });
-
-    this.anims.create({
-        key: 'jumpLeft',
-        frames: this.anims.generateFrameNumbers('characterLeft', { start: 8, end: 13 }),
-        frameRate: 15,
+        key: 'fall',
+        frames: this.anims.generateFrameNumbers('character', { start: 22, end: 23 }),
+        frameRate: 10,
         repeat: 0
     });
 
@@ -109,7 +97,7 @@ function create() {
     camera.startFollow(player);
     camera.setBounds(0, 0, 14000, 216);
 
-    groundLayer.setCollisionByExclusion([-1])
+    groundLayer.setCollisionByExclusion([-1]);
     this.physics.add.collider(player, groundLayer);
     player.setCollideWorldBounds(false);
 
@@ -128,34 +116,69 @@ function create() {
     controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
 }
 
-function update(time, delta) {
-    controls.update(delta);
-
+function update() {
     if (cursors.left.isDown) {
-        direction = 'left';
         player.setVelocityX(-160);
+        player.flipX = true;
 
-        player.anims.play('left', true);
+        if (isJumping == true) {
+            player.anims.play('jump', true);
+            player.on(Phaser.Animations.Events.SPRITE_ANIMATION_KEY_COMPLETE + 'jump', function() {
+                isFalling = true;
+            });
+
+            if (isJumping == true && player.body.onFloor()) {
+                player.anims.play('slide', true);
+                isFalling = false;
+                this.tweens.add({
+                    targets: player,
+                    alpha: '+=1',
+                    duration: 250,
+                    onComplete: function() { isJumping = false; },
+                });
+            }
+        } else {
+            player.anims.play('run', true);
+        }
     } else if (cursors.right.isDown) {
-        direction = 'right';
+        player.flipX = false;
         player.setVelocityX(160);
 
         if (isJumping == true) {
-            player.anims.play('jumpRight', true);
+            player.anims.play('jump', true);
+            player.on(Phaser.Animations.Events.SPRITE_ANIMATION_KEY_COMPLETE + 'jump', function() {
+                isFalling = true;
+            });
+
+            if (isJumping == true && player.body.onFloor()) {
+                player.anims.play('slide', true);
+                isFalling = false;
+                this.tweens.add({
+                    targets: player,
+                    alpha: '+=1',
+                    duration: 250,
+                    onComplete: function() { isJumping = false; },
+                });
+            }
         } else {
-            player.anims.play('right', true);
+            player.anims.play('run', true);
         }
     } else {
         player.setVelocityX(0);
-        if (direction == 'right') {
-            player.anims.play('idleRight');
-        } else if (direction == 'left') {
-            player.anims.play('idleLeft');
-        }
+        player.anims.play('idle');
     }
+
+    if (isFalling == true) {
+        player.anims.play('fall', true);
+    }
+
 
     if (cursors.space.isDown && player.body.onFloor()) {
         isJumping = true;
         player.setVelocityY(-200);
+    }
+
+    if (player.body.onFloor()) {
+        isFalling = false;
     }
 }
