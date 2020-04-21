5a80de009346af85e3cc3dfade4280aa8ea9ac4a
Felicien <felicienfouillet@live.fr>
2020-04-21 11:35:47 +0200

    evo: basic player movements and animations + addition of tilemap with collisions

--- Godot/scripts/Player.gd
+++ Godot/scripts/Player.gd
@@ -1,40 +1,37 @@
 extends KinematicBody2D
 
-var screen_size
-
 var anim_player
 
-const SPEED = 250
-const JUMP = 500
-const GRAVITY = 50
-const MAX_FALL_VELOCITY = 500
+export var SPEED = 200
+export var JUMP = 275
+export var GRAVITY = 1000
 
-var y_velocity = 0
+var velocity = Vector2()
 var facing_right = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-#	screen_size = get_viewport_rect().size
 	anim_player =  $AnimatedSprite
 	
 func _physics_process(delta):
 	var dir = 0
+	
+	velocity.y += delta * GRAVITY
 
 	if Input.is_action_pressed("ui_left"):
 		dir -=1
+		velocity.x = -SPEED
 	elif Input.is_action_pressed("ui_right"):
 		dir +=1
-		
-	move_and_slide(Vector2(dir * SPEED, y_velocity), Vector2(0, -1))
+		velocity.x = SPEED
+	else:
+		velocity.x = 0
 
 	var onFloor = is_on_floor();
+	
 	if onFloor and Input.is_action_pressed("ui_up"):
-		y_velocity -= JUMP
-	if onFloor and y_velocity >= 5:
-		y_velocity = 5
-	if y_velocity > MAX_FALL_VELOCITY:
-		y_velocity = MAX_FALL_VELOCITY
+		velocity.y = -JUMP
 
 	if facing_right and dir > 0:
 		flipSprite()
@@ -48,6 +45,12 @@ func _physics_process(delta):
 			PlayAnim('run')
 	else:
 		PlayAnim('jump')
+		
+	move_and_slide(velocity, Vector2(0, -1))
+	
+	if position.y >= 500:
+		position.x = -55
+		position.y = 8
 
 func flipSprite():
 	facing_right = !facing_right
