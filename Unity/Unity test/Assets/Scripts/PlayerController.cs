b7af0afd3e7e8ed3d6ae3955fcdb736ab71dddaa
Felicien <felicienfouillet@live.fr>
2020-04-23 11:43:27 +0200

    evo: first step with player movements

--- Unity/Unity test/Assets/Scripts/PlayerController.cs
+++ Unity/Unity test/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    /*public float speed;*/
+    public float maxSpeed = 2f;
+    bool facingLeft = false;
+    private Animator anim;
+
+    private Rigidbody2D rb2d;
+
+    Vector3 Velocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float move = Input.GetAxis("Horizontal");
+
+        rb2d.velocity = new Vector3(move * maxSpeed, rb2d.velocity.y);
+
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        if (move < 0 && !facingLeft)
+        {
+            Flip();
+        }
+        else if (move > 0 && facingLeft)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingLeft = !facingLeft;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
